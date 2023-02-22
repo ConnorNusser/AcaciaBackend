35c6a5ab4b628c6aa57c93fc437234b9b53195a9
Connor Nusser <connornusser@gmail.com>
2023-02-21 21:12:35 -0600

    fpasdoads

---
+++ build/src/routes/openai.js
@@ -0,0 +1,30 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const openai_1 = require("openai");
+const router = express_1.default.Router();
+const OpenApiKey = process.env.OPENAI_API_KEY;
+const configuration = new openai_1.Configuration({
+    organization: "org-qRPRbD0Nm3qbT9gVq9ccynM1",
+    apiKey: OpenApiKey,
+});
+const openai = new openai_1.OpenAIApi(configuration);
+const feedObj = [
+    {
+        "prompt": "content"
+    }
+];
+router.get('/', async (req, res) => {
+    const response = await openai.createCompletion({
+        model: "text-babbage-001",
+        prompt: `Hi how's it going?`,
+        max_tokens: 7,
+        temperature: 0,
+    });
+    res.send(response.data);
+});
+exports.default = router;
+//# sourceMappingURL=openai.js.map
