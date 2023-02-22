107c598e95d60b729b3878a4f2d1ff375df4f496
Connor Nusser <connornusser@gmail.com>
2023-02-21 20:35:09 -0600

    commit all

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,8 @@
 import express from 'express';
-import openAiRoutes from '../routes/openai';
+import openAiRoutes from './routes/openai';
 const app = express();
 const port = 8000;
-console.log(require("dotenv").config())
-console.log("hi");
 require('dotenv').config();
-console.log(process.env.OPENAI_API_KEY)
 app.get('/', (req, res) => res.send("Hello from homepage"));
 app.use('/getfeedinfo', openAiRoutes);
 
