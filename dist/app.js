6e13eab22af4c17a1db6c68ee3dabbc97d9e6c03
Connor Nusser <connornusser@gmail.com>
2023-02-21 20:50:04 -0600

    change

--- dist/app.js
+++ dist/app.js
@@ -8,9 +8,9 @@ const openai_1 = __importDefault(require("./routes/openai"));
 const app = (0, express_1.default)();
 const port = 8000;
 require('dotenv').config();
+app.get('/', (req, res) => res.send("Hello from 2323homepage"));
+app.use('/getfeedinfo', openai_1.default);
 app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
 });
-app.get('/', (req, res) => res.send("Hello from 2323homepage"));
-app.use('/getfeedinfo', openai_1.default);
 //# sourceMappingURL=app.js.map
