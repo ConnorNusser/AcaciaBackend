1bae85464559279eca451ec53a78485f3b4c925e
Connor <connornusser@gmail.com>
2024-02-27 02:32:51 -0600

    dsa

--- build/src/routes/instagram.js
+++ build/src/routes/instagram.js
@@ -29,8 +29,6 @@ const postToInsta = async (imageUrl, caption, igUserName, igPassword) => {
 };
 router.post('/', async (req, res) => {
     const promptInfo = req.body;
-    console.log(req.body);
-    console.log("------EOF")
     try {
         const response = await postToInsta(promptInfo.imageUrl, promptInfo.caption, promptInfo.igUserName, promptInfo.igPassword);
         if (response) {
